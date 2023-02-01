1fe0b37af4feb7a7577f518621b5a3e8adcd03e4
Homer Rivera <homer.rivera@gmail.com>
2023-02-01 08:19:49 +0000

    added option for https if using proxy

--- socioboard-web-php/server.php
+++ socioboard-web-php/server.php
@@ -7,6 +7,10 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
